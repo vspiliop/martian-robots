86bfafb43593a2e6ab1268635fe8e01dd0095c07
Vassilis Spiliopoulos <2billy@gmail.com>
2020-08-08 13:10:25 +0300

    factory can create chain of instructions

---
+++ src/main/java/robot/actions/InstructionsChainFactory.java
@@ -0,0 +1,28 @@
+package robot.actions;
+
+import robot.Robot;
+
+import java.util.function.Consumer;
+
+import static robot.actions.moving.ProceedForwardInstruction.forwardInstruction;
+import static robot.actions.turning.TurnLeftInstruction.leftInstruction;
+import static robot.actions.turning.TurnRightInstruction.rightInstruction;
+
+public class InstructionsChainFactory {
+
+    public static Consumer<Robot> instructionChain(String line) {
+        if(line.length() > 100) {
+            throw new IllegalArgumentException("Instruction chain cannot be longer than 100");
+        }
+
+        return line.chars().mapToObj((int instruction)-> {
+            if (instruction == 'F')
+                return forwardInstruction();
+            if (instruction == 'R')
+                return rightInstruction();
+            if (instruction == 'L')
+                return leftInstruction();
+            throw new IllegalArgumentException("Unknown command");
+        }).reduce((Robot r) -> { }, Consumer::andThen);
+    }
+}
---
+++ src/test/java/FactoryCanCreateInstructionChains.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+import static robot.actions.InstructionsChainFactory.instructionChain;
+
+public class FactoryCanCreateInstructionChains {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void maxInstructionChainLimitReached() {
+        instructionChain(createInstructionsStringOfLength(101));
+    }
+
+    @Test
+    public void maxInstructionChainLimitNitReached() {
+        instructionChain(createInstructionsStringOfLength(100));
+    }
+
+    private String createInstructionsStringOfLength(int length) {
+        return IntStream.rangeClosed(1, length).mapToObj(i -> "F").reduce(String::concat).get();
+    }
+
+}
